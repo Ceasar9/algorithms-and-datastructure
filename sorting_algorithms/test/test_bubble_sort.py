059f46507e73472b7214d27a33b96b98aefae35e
Behzad Sezari <16059626+Ceasar9@users.noreply.github.com>
2024-09-11 17:56:26 +0200

    add 'test_bubble_sort' for 'BubbleSort' algorithm.

--- sorting_algorithms/test/test_bubble_sort.py
+++ sorting_algorithms/test/test_bubble_sort.py
@@ -1,9 +1,26 @@
 from sorting_algorithms.BubbleSort import BubbleSort
 
 
-def test__bubble_sort():
+def test__bubble_sort_numeric_values():
     data = [1, 5, 234, 123, 2, 32, 500, 23, 2,96]
     bs_obj = BubbleSort()
     sorted_data = bs_obj.sort(data)
     
     assert sorted_data == [1, 2, 2, 5, 23, 32, 96, 123, 234, 500]
+
+
+def test__bubble_sort_string_values():
+    data = ["Behzad", "behzad", "BEHZAD"]
+    bs_obj = BubbleSort()
+    sorted_data = bs_obj.sort(data)
+    
+    assert sorted_data == ["BEHZAD", "Behzad", "behzad"]
+    
+    
+def test__bubble_sort_consistent_listElements_type():
+    data = [1,2,22,"behzad",33]
+    
+    bs_obj = BubbleSort()
+    sorted_data = bs_obj.sort(data)
+    
+    assert sorted_data == (f"ERROR: Cannot sort the list. Type inconsistency detected... the list does not contain homogeneous type for all elements in the list: {data}")
